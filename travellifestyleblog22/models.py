31df73dac7fa22543c2dab1af1df9afcad492834
biareiam <biareiam@gmail.com>
2022-02-23 08:02:43 +0000

    adding the article detail page and nav bar

--- travellifestyleblog22/models.py
+++ travellifestyleblog22/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="What's up? Travel and Lifestyle blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
