9363b9398be3141e7b4131977a5cf6ec8876a22f
biareiam <biareiam@gmail.com>
2022-03-09 13:24:42 +0000

    Starting the readme file

--- members/forms.py
+++ members/forms.py
@@ -6,17 +6,15 @@ from travellifestyleblog22.models import Profile
 
 
 class ProfilePageForm(forms.ModelForm):
-    """ adjust form style """
     class Meta:
-        """ Adjust bootstrap style """
         model = Profile
-        fields = ('bio', 'profile_pic', 'instagram_url', 'facebook_url', 'twitter_url')
+        fields = ('bio', 'profile_pic', 'facebook_url', 'twitter_url', 'instagram_url')
         widgets = {
-            'bio': forms.Textarea(attrs={'class': 'form-control'}),
-            'instagram_url': forms.TextInput(attrs={'class': 'form-control'}),
-            'facebook_url': forms.TextInput(attrs={'class': 'form-control'}),
-            'twitter_url': forms.TextInput(attrs={'class': 'form-control'}),
-            }
+                'bio': forms.Textarea(attrs={'class': 'form-control'}),
+                'facebook_url': forms.TextInput(attrs={'class': 'form-control'}),
+                'twitter_url': forms.TextInput(attrs={'class': 'form-control'}),
+                'instagram_url': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class SignUpForm(UserCreationForm):
